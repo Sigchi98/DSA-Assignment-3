3651e8e181135a53288f3e1632eaf1a26108e4a9
Mohit Sigchi <50646753+Sigchi98@users.noreply.github.com>
2023-06-28 17:53:26 +0530

    Create question_no_5.cpp

---
+++ question_no_5.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+    int n = digits.size();
+    int carry = 1;
+    for (int i = n - 1; i >= 0; i--) {
+        digits[i] += carry;
+        carry = digits[i] / 10;
+        digits[i] %= 10;
+        
+        if (carry == 0) {
+            break;
+        }
+    }
+    
+    if (carry > 0) {
+        digits.insert(digits.begin(), carry);
+    }
+    
+    return digits;
+    }
+};
